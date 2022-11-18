ca6c8a14667793bedb5c1aa0aa2442866710ea5e
Yang xiaoyu <85976753+sddzcuigc@users.noreply.github.com>
2022-11-18 15:38:27 +0800

    Update main.js

--- main.js
+++ main.js
@@ -10,27 +10,29 @@
     function sleep (time) {
         return new Promise((resolve) => setTimeout(resolve, time));
     }
-
-    // 运行试试
-    if (window.location.href.indexOf("online-class") != -1) {
-        var intnum = Math.floor(Math.random() * 10 );
-
-        //有的时候100%了还是有再次学习的时候
-        sleep(3000).then(() => {if (document.getElementsByClassName('cell fs-tooltip')[4+6*intnum].textContent.endsWith("100%") == false){
-            document.getElementsByClassName("group")[intnum].children[0].click();}
-                                else {document.location.reload()}})
+    sleep(3000).then(() => {
+        // 选课页面
+        if (window.location.href.indexOf("online-class") != -1) {//判断是选课页面还是听课页面
 
 
+            //有的时候100%了还是有再次学习的时候
+            sleep(3000).then(() => {var pagenum = document.getElementsByClassName('number').length;
+                                    document.getElementsByClassName('number')[pagenum-1].click();sleep(3000).then(() => {var intnum = Math.floor(Math.random() * (document.getElementsByClassName('fs-table__row').length) );
+                                    if (document.getElementsByClassName('cell fs-tooltip')[4+6*intnum].textContent.endsWith("100%") == false){
+                                        document.getElementsByClassName("group")[intnum].children[0].click();}
+                                    else {document.location.reload()}})})
 
-    }
-    else {
-        sleep(3000).then(() => {document.getElementsByTagName("video")[0].muted = true;
-                                document.getElementsByTagName("video")[0].play();
-                                document.getElementsByTagName("video")[0].addEventListener("ended",function(){window.close();opener.location.reload();})})
-    };
+
+
+        }
+        else {//听课页面
+            sleep(3000).then(() => {document.getElementsByTagName("video")[0].muted = true;
+                                    document.getElementsByTagName("video")[0].play();
+                                    document.getElementsByTagName("video")[0].addEventListener("ended",function(){sleep(10000).then(() => {window.close();opener.location.reload();})})})
+        };});
 }
 
-
+setTimeout(function(){},10000);
 
 
 
